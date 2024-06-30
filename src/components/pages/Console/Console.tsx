6a86bafe7ea520a0de90ff3817ca5a46fc170e85
mangelvl <thorristerki@gmail.com>
2024-06-29 22:16:53 -0400

    comsole color

--- src/components/pages/Console/Console.tsx
+++ src/components/pages/Console/Console.tsx
@@ -1,7 +1,7 @@
 export function Console() {
   return (
     <>
-      <div className="flex flex-col bg-slate-300 w-full sm:w-[430px] h-full rounded-3xl gap-4 p-5">
+      <div className="flex flex-col bg-[#6404D7] w-full sm:w-[430px] h-full rounded-3xl border-[3px] border-black p-5">
 
         <div id="pantalla" className='bg-black rounded-t-xl rounded-b-3xl p-4'>
           <div className='flex items-center justify-center bg-slate-600 rounded-md w-full h-72'>
@@ -14,73 +14,74 @@ export function Console() {
 
         </div>
 
-        <div id='controles' className='h-[288px] py-4'>
-          <div className='flex flex-col items-center justify-center w-full gap-4'>
-            <div className="w-full flex flex-row justify-between">
-              <div className='flex items-center justify-center w-[62px] h-[32px] bg-white rounded-b-3xl rounded-t-lg rounded-r-lg border border-black'>
-                <p className=''>L</p>
+        <div id='controles' className=''>
+          <div className='flex flex-col items-center justify-center w-full gap-8'>
+
+            <div className="w-full flex flex-row justify-between px-4 mt-1">
+              <div className='flex items-center justify-center w-20 h-10 bg-[#2B2B2B] rounded-b-3xl rounded-t-lg rounded-r-lg border-[3px] border-black'>
+                <p className='text-gray-200 text-lg font-medium'>L</p>
               </div>
 
-              <div className='flex items-center justify-center w-[62px] h-[32px] bg-white rounded-b-3xl rounded-t-lg rounded-l-lg border border-black'>
-                <p>R</p>
+              <div className='flex items-center justify-center w-20 h-10 bg-[#2B2B2B] rounded-b-3xl rounded-t-lg rounded-l-lg border-[3px] border-black'>
+                <p className='text-gray-200 text-lg font-medium'>R</p>
               </div>
             </div>
 
             <div className="flex flex-row items-center justify-around w-full">
-              <div className="flex flex-col items-center justify-center w-[94px] h-[94px]">
-                <div className='flex items-center justify-center w-[32px] h-[32px] bg-white rounded-t border border-black border-b-white'>
-                  <p>T</p>
+              <div className="flex flex-col items-center justify-center w-32 h-32">
+                <div className='flex items-center justify-center w-10 h-10 bg-[#2B2B2B] rounded-t border-[3px] border-black border-b-[#2B2B2B]'>
+                  <p className='text-gray-200 text-lg font-medium'>T</p>
                 </div>
 
-                <div className='flex flex-row'>
-                  <div className='flex items-center justify-center w-[32px] h-[32px] bg-white rounded-l border border-black border-r-white'>
-                    <p>L</p>
+                <div id='cruz' className='flex flex-row'>
+                  <div className='flex items-center justify-center w-10 h-10 bg-[#2B2B2B] rounded-l border-[3px] border-black border-r-[#2B2B2B]'>
+                    <p className='text-gray-200 text-lg font-medium'>L</p>
                   </div>
 
-                  <div className='w-[32px] h-[32px] bg-white border border-white'></div>
+                  <div className='w-10 h-10 bg-[#2B2B2B] border-[3px] border-[#2B2B2B]'></div>
 
-                  <div className='flex items-center justify-center w-[32px] h-[32px] bg-white rounded-r border border-black border-l-white'>
-                    <p>R</p>
+                  <div className='flex items-center justify-center w-10 h-10 bg-[#2B2B2B] rounded-r border-[3px] border-black border-l-[#2B2B2B]'>
+                    <p className='text-gray-200 text-lg font-medium'>R</p>
                   </div>
                 </div>
 
-                <div className='flex items-center justify-center w-[32px] h-[32px] bg-white rounded-b border border-black border-t-white'>
-                  <p>B</p>
+                <div className='flex items-center justify-center w-10 h-10 bg-[#2B2B2B] rounded-b border-[3px] border-black border-t-[#2B2B2B]'>
+                  <p className='text-gray-200 text-lg font-medium'>B</p>
                 </div>
               </div>
 
-              <div className='flex flex-col items-center justify-center w-[142px] h-[142px] border rounded-full'>
-                <div className='flex items-center justify-center w-[42px] h-[42px] bg-white rounded-full border border-black'>
-                  <p>X</p>
+              <div id='botones' className='flex flex-col items-center justify-center w-36 h-36 rounded-full bg-[#4F08AC]'>
+                <div className='flex items-center justify-center w-10 h-10 bg-[#0649B2] rounded-full border-[3px] border-black'>
+                  <p className='text-gray-200 text-lg font-medium'>X</p>
                 </div>
 
-                <div className='flex flex-row gap-[42px]'>
-                  <div className='flex items-center justify-center w-[42px] h-[42px] bg-white rounded-full border border-black'>
-                    <p>Y</p>
+                <div className='flex flex-row gap-10'>
+                  <div className='flex items-center justify-center w-10 h-10 bg-[#009740] rounded-full border-[3px] border-black'>
+                    <p className='text-gray-200 text-lg font-medium'>Y</p>
                   </div>
 
-                  <div className='flex items-center justify-center w-[42px] h-[42px] bg-white rounded-full border border-black'>
-                    <p>A</p>
+                  <div className='flex items-center justify-center w-10 h-10 bg-[#E81B22] rounded-full border-[3px] border-black'>
+                    <p className='text-gray-200 text-lg font-medium'>A</p>
                   </div>
                 </div>
 
-                <div className='flex items-center justify-center w-[42px] h-[42px] bg-white rounded-full border border-black'>
-                  <p>B</p>
+                <div className='flex items-center justify-center w-10 h-10 bg-[#FECD19] rounded-full border-[3px] border-black'>
+                  <p className='text-gray-200 text-lg font-medium'>B</p>
                 </div>
               </div>
             </div>
 
-            <div className="flex flex-row justify-around w-full">
-              <div className='flex items-center justify-center w-[62px] bg-white rounded-full border border-black'>
-                <p>Start</p>
+            <div id='menu' className="flex flex-row justify-center gap-4 w-full mt-10 mb-14">
+              <div className='flex items-center justify-center w-[62px] bg-[#2B2B2B] rounded-full border-[3px] border-black'>
+                <p className='text-gray-200 text-xs font-medium'>Start</p>
               </div>
 
-              <div className='flex items-center justify-center w-[62px] bg-white rounded-full border border-black'>
-                <p>Menu</p>
+              <div className='flex items-center justify-center w-[62px] bg-[#2B2B2B] rounded-full border-[3px] border-black'>
+                <p className='text-gray-200 text-xs font-medium'>Menu</p>
               </div>
 
-              <div className='flex items-center justify-center w-[62px] bg-white rounded-full border border-black'>
-                <p>Select</p>
+              <div className='flex items-center justify-center w-[62px] bg-[#2B2B2B] rounded-full border-[3px] border-black'>
+                <p className='text-gray-200 text-xs font-medium'>Select</p>
               </div>
             </div>
           </div>
